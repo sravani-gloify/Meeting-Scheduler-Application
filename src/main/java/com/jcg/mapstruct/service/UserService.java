adf5640fc4d17e3dd5aabb07743b6ad5f248f5ab
sravani-gloify <124998513+sravani-gloify@users.noreply.github.com>
2023-03-29 12:54:03 +0530

    Update UserService.java

--- src/main/java/com/jcg/mapstruct/service/UserService.java
+++ src/main/java/com/jcg/mapstruct/service/UserService.java
@@ -10,7 +10,7 @@ import com.jcg.mapstruct.dto.UserDto;
 import com.jcg.mapstruct.mapper.UserMapper;
 import com.jcg.mapstruct.model.User;
 import com.jcg.mapstruct.repository.UserRepository;
-
+//defining the bussiness logic
 @Service
 public class UserService {
 	@Autowired
@@ -18,6 +18,7 @@ public class UserService {
 	@Autowired 
 	private UserMapper userMapper;
 	
+	//save a specific record by using the method save() of CrudRepository  
 	 public UserDto save(UserDto userDto) {
 	   User user  = userMapper.dtoToModel(userDto);
 	   User userEntity= repository.save(user);
@@ -25,28 +26,33 @@ public class UserService {
 	   return userDto;
 	       
 	    }
-
+     //getting all record by using the method findAll() of CrudRepository  
 	    public List<User> findAll() {
 	     
 	        return repository.findAll();
 	    }
-
+ //getting a specific record by using the method findById() of CrudRepository  
 	    public UserDto findOne(Long id) {
 	       
 	       User user=repository.findById(id).orElseThrow(()->new ResourceNotFoundException("Resource NotFound",HttpStatus.NOT_FOUND));
 	       UserDto userDto=userMapper.modelToDto(user);
 	        return userDto;
 	    }
-
+       //getting a specific record by using the method findUserByEmailId() of CrudRepository  
 		public User findUserByEmailId(String emailId) {
 			return repository.findByEmailId(emailId);
-		}  
+		} 
+	//getting a specific record by using the method getUser() of CrudRepository  
       public List<UserDto> getUsers(){
     	  List<User> user=repository.findAll();
     	  List<UserDto> userDtos=userMapper.modelToDto(user);
 		return userDtos;
 	   
       }
+	
+	
+	
+	//Getter And Setters
 		public UserRepository getRepository() {
 			return repository;
 		}
