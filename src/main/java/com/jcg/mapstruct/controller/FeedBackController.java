e27d1f7ba0e3141b089b60a6201a3c1e7ba8940b
sravani-gloify <124998513+sravani-gloify@users.noreply.github.com>
2023-03-29 13:15:13 +0530

    Update FeedBackController.java

--- src/main/java/com/jcg/mapstruct/controller/FeedBackController.java
+++ src/main/java/com/jcg/mapstruct/controller/FeedBackController.java
@@ -9,25 +9,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jcg.mapstruct.model.FeedBack;
 import com.jcg.mapstruct.service.FeedBackService;
-
+//mark ascontroller
 @RestController
 @RequestMapping("/FeedBack")
 public class FeedBackController {
 	@Autowired
 	private FeedBackService feedBackService;
-	
+	//simple api
 	@GetMapping("/fb")
 	public String getFeedBack()
 	{
 		return "FeedBack";
 		
 	}
+	//creating a get mapping that retrieves the detail of a specific book
 	@PostMapping("/save")
 	public FeedBack CreateFeedBack(@RequestBody FeedBack feedBack)
 	{
 		return feedBackService.addFeedBack(feedBack);
 		
 	}
+	
+	
 	public FeedBackService getFeedBackService() {
 		return feedBackService;
 	}
