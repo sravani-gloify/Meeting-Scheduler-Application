6f30156149428dae3e8fdd30b61aa92d12289d71
sravani-gloify <124998513+sravani-gloify@users.noreply.github.com>
2023-03-29 13:04:39 +0530

    Update CandidateController.java

--- src/main/java/com/jcg/mapstruct/controller/CandidateController.java
+++ src/main/java/com/jcg/mapstruct/controller/CandidateController.java
@@ -15,35 +15,37 @@ import com.jcg.mapstruct.dto.CandidateDto;
 import com.jcg.mapstruct.model.Candidate;
 import com.jcg.mapstruct.service.CandidateService;
 
-
+// creating basic mapping path
 @RequestMapping("/candidate")
+//maks as controller
 @RestController
 public class CandidateController {
 	
 	@Autowired
 	private CandidateService candidateService;
-	
+	//creating post mapping that post the candidate detail in the database 
 	@PostMapping("/save")
 	public HttpEntity<CandidateDto> CreateCandidate(@RequestBody CandidateDto candidateDto)
 	{
 		CandidateDto candidate=candidateService.createCandidate(candidateDto);
 		return new ResponseEntity<>(candidate,HttpStatus.CREATED);
 		
-	}	
+	}
+	//creating a get mapping that retrieves the detail of a specific candidate
 	@GetMapping("/{id}")
 	public HttpEntity<CandidateDto> getCandidate(@PathVariable Long id)
 	{
 		CandidateDto candidate=candidateService.findOne(id);
 		return new ResponseEntity<>(candidate,HttpStatus.CREATED);
 		
-	}	
+	}	//creating a get mapping that retrieves the detail of a specific candidate by emailid
 	  @GetMapping
 	  public Candidate findByEmail(String Email)
 	  {
 		return candidateService.findCadidateByEmail(Email);
 		  
 	  }  
-	
+	//creating a get mapping that retrieves the detail of all candidates
 	@GetMapping("/getAll")
 	public List<Candidate> getCandidate()
 	{
