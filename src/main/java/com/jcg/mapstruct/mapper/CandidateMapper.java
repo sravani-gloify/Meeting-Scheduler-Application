69c3becb307cb0b0cc1a1837b28b978b2a6feb66
sravani-gloify <124998513+sravani-gloify@users.noreply.github.com>
2023-03-29 13:28:06 +0530

    Update CandidateMapper.java

--- src/main/java/com/jcg/mapstruct/mapper/CandidateMapper.java
+++ src/main/java/com/jcg/mapstruct/mapper/CandidateMapper.java
@@ -7,13 +7,13 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.jcg.mapstruct.dto.CandidateDto;
 import com.jcg.mapstruct.model.Candidate;
-
+//mark as mapper
 @Mapper(componentModel = "spring")
 public interface CandidateMapper {
-	
+	//create the bean mapping
 	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 	CandidateDto toCandidateDto(Candidate  candidate);
-
+      
 	@InheritInverseConfiguration
 	Candidate toCandidate(CandidateDto candidateDto);
     
