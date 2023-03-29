d37d23543987cd3d637889bc95552020f00b9d8a
sravani-gloify <124998513+sravani-gloify@users.noreply.github.com>
2023-03-29 13:07:04 +0530

    Update UserController.java

--- src/main/java/com/jcg/mapstruct/controller/UserController.java
+++ src/main/java/com/jcg/mapstruct/controller/UserController.java
@@ -14,27 +14,30 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jcg.mapstruct.dto.UserDto;
 import com.jcg.mapstruct.model.User;
 import com.jcg.mapstruct.service.UserService;
-
+//mark as controoler
 @RestController
+//create the basic path for mapping
 @RequestMapping("/user")
 public class UserController {
 	
 	@Autowired
 	private UserService service;
 	
-       
+       //creating a put mapping that save the detail of a User 
 	@PostMapping("/save")
 	public HttpEntity<UserDto> createUser(@RequestBody UserDto userDto){
 		userDto=service.save(userDto);
 		return new ResponseEntity<>(userDto,HttpStatus.CREATED);
 		
 	}
+	//creating a get mapping that retrieves the detail of a specific user
 	@GetMapping("/{id}")
 	public HttpEntity<UserDto> getUser(@PathVariable Long id){
 		UserDto userDto=service.findOne(id);
 		return new ResponseEntity<>(userDto,HttpStatus.CREATED);
 		
 	}
+	//creating a get mapping that retrieves the detail of a specific user by emailid
 	@GetMapping
 	public User findByUserEmailId( String EmailId)
 	{
