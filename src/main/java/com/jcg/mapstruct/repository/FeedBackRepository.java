ebe8362eb65f239d54cc8c48230742a3e16b9e19
sravani-gloify <124998513+sravani-gloify@users.noreply.github.com>
2023-03-29 12:55:31 +0530

    Update FeedBackRepository.java

--- src/main/java/com/jcg/mapstruct/repository/FeedBackRepository.java
+++ src/main/java/com/jcg/mapstruct/repository/FeedBackRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jcg.mapstruct.model.FeedBack;
+
+//repository that extends CrudRepository  
 @Repository
 public interface FeedBackRepository extends JpaRepository<FeedBack,Long>{
 
