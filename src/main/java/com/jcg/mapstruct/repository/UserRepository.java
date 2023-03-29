53b03e3261e8ebadba02e554bf77eccedc86dc93
sravani-gloify <124998513+sravani-gloify@users.noreply.github.com>
2023-03-29 12:56:07 +0530

    Update UserRepository.java

--- src/main/java/com/jcg/mapstruct/repository/UserRepository.java
+++ src/main/java/com/jcg/mapstruct/repository/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jcg.mapstruct.model.User;
-
+//repository that extends CrudRepository  
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
